ce38532611febb2cb0280071028216033db36fe6
Guillaume Dumas <gdumas@z3r10p1.42lyon.fr>
2024-10-28 18:05:09 +0100

    fix: added delay and protection for file write/read data race

--- scraper/db.py
+++ scraper/db.py
@@ -9,164 +9,177 @@ import csv
 from decimal import Decimal
 
 def clear_file(file_path):
-    try:
-        os.remove(file_path)
-        print(f"File {file_path} has been deleted.")
-    except FileNotFoundError:
-        print(f"File {file_path} not found.")
-    except PermissionError:
-        print(f"Permission denied: Unable to delete {file_path}.")
-    except Exception as e:
-        print(f"An error occurred while trying to delete {file_path}: {e}")
+	try:
+		os.remove(file_path)
+		print(f"File {file_path} has been deleted.")
+	except FileNotFoundError:
+		print(f"File {file_path} not found.")
+	except PermissionError:
+		print(f"Permission denied: Unable to delete {file_path}.")
+	except Exception as e:
+		print(f"An error occurred while trying to delete {file_path}: {e}")
 
 def update_fighter_stats(winner, loser, headers):
-  requests.put(f'http://backend:8000/api/fighters/update_elo/',
-                json={"winner_id": winner["f_id"], "loser_id": loser["f_id"]},
-                headers=headers).raise_for_status()
-  requests.put(f'http://backend:8000/api/fighters/update_fighter/',
-                json={"f_id": winner["f_id"], "win": True}, headers=headers).raise_for_status()
-  requests.put(f'http://backend:8000/api/fighters/update_fighter/',
-                json={"f_id": loser["f_id"], "win": False}, headers=headers).raise_for_status()
+	requests.put(f'http://backend:8000/api/fighters/update_elo/',
+				json={"winner_id": winner["f_id"], "loser_id": loser["f_id"]},
+				headers=headers).raise_for_status()
+	requests.put(f'http://backend:8000/api/fighters/update_fighter/',
+				json={"f_id": winner["f_id"], "win": True}, headers=headers).raise_for_status()
+	requests.put(f'http://backend:8000/api/fighters/update_fighter/',
+				json={"f_id": loser["f_id"], "win": False}, headers=headers).raise_for_status()
 
 def handle_bets_open(red_fighter, blue_fighter, headers):
-    response_red, response_blue, match = None, None, None
-    try:
-        response_red = requests.post('http://backend:8000/api/fighters/', json={"name": red_fighter.replace(" ", "_")}, headers=headers)
-        response_red.raise_for_status()
-        response_blue = requests.post('http://backend:8000/api/fighters/', json={"name": blue_fighter.replace(" ", "_")}, headers=headers)
-        response_blue.raise_for_status()
-        fighter_red = response_red.json()
-        fighter_blue = response_blue.json()
-        response_match = requests.post('http://backend:8000/api/matches/',
-                                       json={"red_id": fighter_red["f_id"], "blue_id": fighter_blue["f_id"]},
-                                       headers=headers)
-        response_match.raise_for_status()
-        match = response_match.json()
-        return fighter_red, fighter_blue, match
-    except Exception as e:
-        message = []
-        message.append("db: bet phase: red: ")
-        if response_red:
-            message.append(f"{response_red.json()}")
-        else:
-            message.append(f"{red_fighter}")
-        if response_blue:
-            message.append(f" - blue: {response_blue.json()}")
-        else:
-            message.append(f" - blue: {blue_fighter}")
-        message.append(f" {e}")
-        send_to_discord(f" {json.dumps(message, indent=4)} {e}")
-        return None, None, None
-    
+	response_red, response_blue, match = None, None, None
+	try:
+		response_red = requests.post('http://backend:8000/api/fighters/', json={"name": red_fighter.replace(" ", "_")}, headers=headers)
+		response_red.raise_for_status()
+		response_blue = requests.post('http://backend:8000/api/fighters/', json={"name": blue_fighter.replace(" ", "_")}, headers=headers)
+		response_blue.raise_for_status()
+		fighter_red = response_red.json()
+		fighter_blue = response_blue.json()
+		response_match = requests.post('http://backend:8000/api/matches/',
+									   json={"red_id": fighter_red["f_id"], "blue_id": fighter_blue["f_id"]},
+									   headers=headers)
+		response_match.raise_for_status()
+		match = response_match.json()
+		return fighter_red, fighter_blue, match
+	except Exception as e:
+		message = []
+		message.append("db: bet phase: red: ")
+		if response_red:
+			message.append(f"{response_red.json()}")
+		else:
+			message.append(f"{red_fighter}")
+		if response_blue:
+			message.append(f" - blue: {response_blue.json()}")
+		else:
+			message.append(f" - blue: {blue_fighter}")
+		message.append(f" {e}")
+		send_to_discord(f" {json.dumps(message, indent=4)} {e}")
+		return None, None, None
+
 def handle_bets_locked(headers, match):
-    try:
-        m_id = match["m_id"]
-        response = requests.get(f'http://backend:8000/api/bets/bets_volume/?m_id={m_id}', headers=headers)
-        response.raise_for_status()
-        data = response.json()
-        total_red = data['total_red']
-        total_blue = data['total_blue']
-        return total_red, total_blue
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching bets volume: {e}")
-        return None, None
-    
-    
+	try:
+		m_id = match["m_id"]
+		response = requests.get(f'http://backend:8000/api/bets/bets_volume/?m_id={m_id}', headers=headers)
+		response.raise_for_status()
+		data = response.json()
+		total_red = data['total_red']
+		total_blue = data['total_blue']
+		return total_red, total_blue
+	except requests.exceptions.RequestException as e:
+		print(f"Error fetching bets volume: {e}")
+		return None, None
+
 def handle_wins(phase, fighter_red, fighter_blue, current_time, match, headers):
-    duration = datetime.now() - current_time
-    duration_str = f"{duration.total_seconds() // 3600:02.0f}:{(duration.total_seconds() % 3600) // 60:02.0f}:{duration.total_seconds() % 60:02.0f}"
-    winner, _, _ = phase["text"].partition("wins!")
-    winner = winner.strip()
-    try:
-        if winner == fighter_red["name"].replace("_", " "):
-            winner = fighter_red
-            loser = fighter_blue
-        else:
-            winner = fighter_blue
-            loser = fighter_red
-        update_fighter_stats(winner, loser, headers)
-        requests.put(f'http://backend:8000/api/matches/update_match/',
-                     json={"match": match["m_id"], "winner": winner["f_id"], "duration": duration_str}, headers=headers).raise_for_status()
-    except requests.exceptions.RequestException as e:
-        send_to_discord(f"db: win phase: red: {fighter_red} - blue: {fighter_blue} - match: {match} - winner: {winner} - duration: {duration_str} - {e}")
-    except Exception as e:
-        send_to_discord(f"db: Unexpected error: {e}")
-        
+	duration = datetime.now() - current_time
+	duration_str = f"{duration.total_seconds() // 3600:02.0f}:{(duration.total_seconds() % 3600) // 60:02.0f}:{duration.total_seconds() % 60:02.0f}"
+	winner, _, _ = phase["text"].partition("wins!")
+	winner = winner.strip()
+	try:
+		if winner == fighter_red["name"].replace("_", " "):
+			winner = fighter_red
+			loser = fighter_blue
+		else:
+			winner = fighter_blue
+			loser = fighter_red
+		update_fighter_stats(winner, loser, headers)
+		requests.put(f'http://backend:8000/api/matches/update_match/',
+					 json={"match": match["m_id"], "winner": winner["f_id"], "duration": duration_str}, headers=headers).raise_for_status()
+	except requests.exceptions.RequestException as e:
+		send_to_discord(f"db: win phase: red: {fighter_red} - blue: {fighter_blue} - match: {match} - winner: {winner} - duration: {duration_str} - {e}")
+	except Exception as e:
+		send_to_discord(f"db: Unexpected error: {e}")
+		
 def handle_payout(headers, match, info):
-    file_path = '/app/history/last_match.json'
-    start_time = time.time()
-    timeout = 30
-    while True:
-        if time.time() - start_time > timeout:
-            printf("db: Payout request timed out after 30 seconds")
-            return
-        try:
-            with open(file_path, 'r') as file:
-                data = json.load(file)
-            bet_response = requests.put('http://backend:8000/api/bets/bet_payout/',
-                                        json=data, headers=headers, timeout=10)
-            bet_response.raise_for_status()
-            user_response = requests.put('http://backend:8000/api/users/user_payout/',
-                                         json=data, headers=headers, timeout=10)
-            user_response.raise_for_status()
-            print(json.dumps(data, indent=2))
-            time.sleep(1)
-            asyncio.run(send_info(info, match["m_id"], headers))
-            clear_file('/app/history/last_match.json')
-            return
-        except FileNotFoundError:
-            time.sleep(1)
-        except json.JSONDecodeError:
-            print("Error decoding JSON from /app/history/last_match.json. Retrying...")
-            time.sleep(1)
-        except requests.Timeout:
-            print("Request timed out. Retrying...")
-        except requests.RequestException as e:
-            error_message = f"db: Error processing payout: {e.response.status_code} {e.response.reason} for url: {e.response.url}"
-            if e.response.text:
-                error_message += f"\nResponse content: {e.response.text[:200]}..."
-            send_to_discord(error_message)
-            return
-        except Exception as e:
-            send_to_discord(f"db: Unexpected error during payout: {str(e)}")
-            return
-            
+	file_path = '/app/history/last_match.json'
+	start_time = time.time()
+	timeout = 30
+
+	while True:
+		if time.time() - start_time > timeout:
+			print("db: Payout request timed out after 30 seconds")
+			return
+		if not os.path.exists(file_path):
+			time.sleep(0.5)
+			continue
+		if os.path.getsize(file_path) == 0:
+			time.sleep(0.5)
+			continue
+		time.sleep(0.5)
+		try:
+			with open(file_path, 'r') as file:
+				data = json.load(file)
+				if not data:
+					time.sleep(0.5)
+					continue
+
+			bet_response = requests.put('http://backend:8000/api/bets/bet_payout/',
+										json=data, headers=headers, timeout=10)
+			bet_response.raise_for_status()
+
+			user_response = requests.put('http://backend:8000/api/users/user_payout/',
+										json=data, headers=headers, timeout=10)
+			user_response.raise_for_status()
+
+			print(json.dumps(data, indent=2))
+			time.sleep(1)
+			asyncio.run(send_info(info, match["m_id"], headers))
+			clear_file('/app/history/last_match.json')
+			return
+		except FileNotFoundError:
+			time.sleep(0.5)
+		except json.JSONDecodeError:
+			print("Error decoding JSON from /app/history/last_match.json. Retrying...")
+			time.sleep(0.5)
+		except requests.Timeout:
+			print("Request timed out. Retrying...")
+		except requests.RequestException as e:
+			error_message = f"db: Error processing payout: {e.response.status_code} {e.response.reason} for url: {e.response.url}"
+			if e.response.text:
+				error_message += f"\nResponse content: {e.response.text[:200]}..."
+			send_to_discord(error_message)
+			return
+		except Exception as e:
+			send_to_discord(f"db: Unexpected error during payout: {str(e)}")
+			return
+
 def process_match_history(file_path):
-    processed_data = []
-    with open(file_path, 'r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            try:
-                processed_bet = {
-                    'bet_id': row['bet_id'],
-                    'payout': float(Decimal(row['payout'])),
-                    'valid_hash': row['valid_hash'],
-                    'invalid_match': row.get('invalid_match') if row.get('invalid_match') != '' else None
-                }
-                processed_data.append(processed_bet)
-            except KeyError as e:
-                print(f"Missing key in CSV row: {e}")
-            except ValueError as e:
-                print(f"Error converting value in CSV row: {e}")
-    return processed_data
+	processed_data = []
+	with open(file_path, 'r') as file:
+		csv_reader = csv.DictReader(file)
+		for row in csv_reader:
+			try:
+				processed_bet = {
+					'bet_id': row['bet_id'],
+					'payout': float(Decimal(row['payout'])),
+					'valid_hash': row['valid_hash'],
+					'invalid_match': row.get('invalid_match') if row.get('invalid_match') != '' else None
+				}
+				processed_data.append(processed_bet)
+			except KeyError as e:
+				print(f"Missing key in CSV row: {e}")
+			except ValueError as e:
+				print(f"Error converting value in CSV row: {e}")
+	return processed_data
 
 def handle_match_history(file_path, headers):
-    processed_data = process_match_history(file_path)
-    if processed_data:
-        try:
-            history = json.loads(json.dumps(processed_data))
-            response = requests.put('http://backend:8000/api/bets/bet_payout/',
-                                    json=history, headers=headers)
-            response.raise_for_status()
-            print("Bet payout processed successfully")
+	processed_data = process_match_history(file_path)
+	if processed_data:
+		try:
+			history = json.loads(json.dumps(processed_data))
+			response = requests.put('http://backend:8000/api/bets/bet_payout/',
+									json=history, headers=headers)
+			response.raise_for_status()
+			print("Bet payout processed successfully")
 
-            response = requests.put('http://backend:8000/api/users/user_totals/',
-                                    headers=headers)
-            response.raise_for_status()
-            print("User totals updated successfully")
-        except json.JSONDecodeError as e:
-            print(f"Error creating valid JSON: {e}")
-        except requests.RequestException as e:
-            print(f"Error sending processed data to API: {e}")
-    else:
-        print("No valid data found in match history")
+			response = requests.put('http://backend:8000/api/users/user_totals/',
+									headers=headers)
+			response.raise_for_status()
+			print("User totals updated successfully")
+		except json.JSONDecodeError as e:
+			print(f"Error creating valid JSON: {e}")
+		except requests.RequestException as e:
+			print(f"Error sending processed data to API: {e}")
+	else:
+		print("No valid data found in match history")
--- scraper/main.py
+++ scraper/main.py
@@ -17,117 +17,117 @@ secret_file = 'scraper_pass'
 
 
 async def twitch_chat_listener():
-    token_data, token_expiry, headers = initialize_token(user, secret_file)
-    current_time = None
-    fighter_red, fighter_blue, match, total_blue, total_red = None, None, None, None, None
-    phase = {"text": None}
-    sync_time = False
-    
-    while True:
-        try:
-            async with websockets.connect(TWITCH_WS_URL) as websocket:
-                await websocket.send(f'NICK {NICK}')
-                await websocket.send(f'CAP REQ :twitch.tv/tags twitch.tv/commands')
-                await websocket.send(f'JOIN #{CHANNEL_NAME}')
-                print(f"Joining channel: #{CHANNEL_NAME}")
-                
-                with ProcessPoolExecutor() as executor:
-                    while True:
-                        try:
-                            token_data, token_expiry, headers = check_and_refresh_token(token_data, token_expiry, headers, user, secret_file)
-                            print(f"Updated token expiry: {token_expiry}")
-                            
-                            message = await asyncio.wait_for(websocket.recv(), timeout=30)  # Add timeout
-                            
-                            if message.startswith('PING'):
-                                await websocket.send('PONG :tmi.twitch.tv')
-                            elif 'PRIVMSG' in message:
-                                username = re.search(r'display-name=([^;]+)', message)
-                                user_id = re.search(r'user-id=(\d+)', message)
-                                room_id = re.search(r'room-id=(\d+)', message)
-                                msg = re.search(r'PRIVMSG #\S+ :(.*)', message)
-                                if username and user_id and room_id and msg:
-                                    username = username.group(1)
-                                    user_id = user_id.group(1)
-                                    room_id = room_id.group(1)
-                                    msg = msg.group(1)
-                                    if user_id == TARGET_USER_ID and room_id == TARGET_ROOM_ID:
-                                        print(f"Target message: {username}: {msg}")
-                                        if "Bets are OPEN" in msg:
-                                            sync_time = True
-                                            phase["text"] = "Bets are OPEN!"
-                                            red_fighter = re.search(r'for (.*?) vs', msg)
-                                            blue_fighter = re.search(r'vs (.*?)!', msg)
-                                            if red_fighter and blue_fighter:
-                                                red_fighter = red_fighter.group(1)
-                                                blue_fighter = blue_fighter.group(1)
-                                                fighter_red, fighter_blue, match = handle_bets_open(red_fighter, blue_fighter, headers)
-                                                if fighter_red and fighter_blue:
-                                                    await send_phase(phase, fighter_red, fighter_blue, total_blue, total_red, match, headers)
-                                        elif "Bets are locked" in msg and sync_time:
-                                            phase["text"] = "Bets are locked"
-                                            current_time = datetime.now()
-                                            total_blue, total_red = handle_bets_locked(headers, match)
-                                            time.sleep(1)
-                                            if total_red == 0 and total_blue > 0 or total_red > 0 and total_blue == 0:
-                                                executor.submit(handle_payout, headers, match, "Refund")
-                                            if fighter_red and fighter_blue:
-                                                await send_phase(phase, fighter_red, fighter_blue, total_blue, total_red, match, headers)
-                                        elif "wins!" in msg and sync_time:
-                                            truncated_msg = msg.split('.')[0] + '.'
-                                            phase["text"] = truncated_msg
-                                            if fighter_red and fighter_blue and current_time and match:
-                                                handle_wins(phase, fighter_red, fighter_blue, current_time, match, headers)
-                                            await send_phase(phase, fighter_red, fighter_blue, total_blue, total_red, match, headers)
-                                            executor.submit(handle_payout, headers, match, "Payout")
+	token_data, token_expiry, headers = initialize_token(user, secret_file)
+	current_time = None
+	fighter_red, fighter_blue, match, total_blue, total_red = None, None, None, None, None
+	phase = {"text": None}
+	sync_time = False
+	
+	while True:
+		try:
+			async with websockets.connect(TWITCH_WS_URL) as websocket:
+				await websocket.send(f'NICK {NICK}')
+				await websocket.send(f'CAP REQ :twitch.tv/tags twitch.tv/commands')
+				await websocket.send(f'JOIN #{CHANNEL_NAME}')
+				print(f"Joining channel: #{CHANNEL_NAME}")
+				
+				with ProcessPoolExecutor() as executor:
+					while True:
+						try:
+							token_data, token_expiry, headers = check_and_refresh_token(token_data, token_expiry, headers, user, secret_file)
+							print(f"Updated token expiry: {token_expiry}")
+							
+							message = await asyncio.wait_for(websocket.recv(), timeout=30)
+							
+							if message.startswith('PING'):
+								await websocket.send('PONG :tmi.twitch.tv')
+							elif 'PRIVMSG' in message:
+								username = re.search(r'display-name=([^;]+)', message)
+								user_id = re.search(r'user-id=(\d+)', message)
+								room_id = re.search(r'room-id=(\d+)', message)
+								msg = re.search(r'PRIVMSG #\S+ :(.*)', message)
+								if username and user_id and room_id and msg:
+									username = username.group(1)
+									user_id = user_id.group(1)
+									room_id = room_id.group(1)
+									msg = msg.group(1)
+									if user_id == TARGET_USER_ID and room_id == TARGET_ROOM_ID:
+										print(f"Target message: {username}: {msg}")
+										if "Bets are OPEN" in msg:
+											sync_time = True
+											phase["text"] = "Bets are OPEN!"
+											red_fighter = re.search(r'for (.*?) vs', msg)
+											blue_fighter = re.search(r'vs (.*?)!', msg)
+											if red_fighter and blue_fighter:
+												red_fighter = red_fighter.group(1)
+												blue_fighter = blue_fighter.group(1)
+												fighter_red, fighter_blue, match = handle_bets_open(red_fighter, blue_fighter, headers)
+												if fighter_red and fighter_blue:
+													await send_phase(phase, fighter_red, fighter_blue, total_blue, total_red, match, headers)
+										elif "Bets are locked" in msg and sync_time:
+											phase["text"] = "Bets are locked"
+											current_time = datetime.now()
+											time.sleep(1)
+											total_blue, total_red = handle_bets_locked(headers, match)
+											if total_red == 0 and total_blue > 0 or total_red > 0 and total_blue == 0:
+												executor.submit(handle_payout, headers, match, "Refund")
+											if fighter_red and fighter_blue:
+												await send_phase(phase, fighter_red, fighter_blue, total_blue, total_red, match, headers)
+										elif "wins!" in msg and sync_time:
+											truncated_msg = msg.split('.')[0] + '.'
+											phase["text"] = truncated_msg
+											if fighter_red and fighter_blue and current_time and match:
+												handle_wins(phase, fighter_red, fighter_blue, current_time, match, headers)
+											await send_phase(phase, fighter_red, fighter_blue, total_blue, total_red, match, headers)
+											executor.submit(handle_payout, headers, match, "Payout")
 
-                        except asyncio.TimeoutError:
-                            print("No message received, sending PING")
-                            await websocket.send('PING :tmi.twitch.tv')
-                        except websockets.exceptions.ConnectionClosed:
-                            print("WebSocket connection closed. Reconnecting...")
-                            await send_to_discord(f": WebSocket connection closed. Reconnecting...{e}")
-                            break
-                        except Exception as e:
-                            print(f"Error processing message: {e}")
-                            await send_to_discord(f"Error in twitch_chat_listener: {e}")
-                            
-        except websockets.exceptions.WebSocketException as e:
-            print(f"WebSocket error: {e}. Reconnecting in 5 seconds...")
-            await send_to_discord(f"WebSocket error: {e}. Reconnecting in 5 seconds...")
-            await asyncio.sleep(5)
-        except Exception as e:
-            print(f"Unexpected error: {e}. Reconnecting in 10 seconds...")
-            await send_to_discord(f"Unexpected error: {e}. Reconnecting in 10 seconds...")
-            await send_to_discord(f"Unexpected error in twitch_chat_listener: {e}")
-            await asyncio.sleep(10)
+						except asyncio.TimeoutError:
+							print("No message received, sending PING")
+							await websocket.send('PING :tmi.twitch.tv')
+						except websockets.exceptions.ConnectionClosed:
+							print("WebSocket connection closed. Reconnecting...")
+							await send_to_discord(f": WebSocket connection closed. Reconnecting...{e}")
+							break
+						except Exception as e:
+							print(f"Error processing message: {e}")
+							await send_to_discord(f"Error in twitch_chat_listener: {e}")
+							
+		except websockets.exceptions.WebSocketException as e:
+			print(f"WebSocket error: {e}. Reconnecting in 5 seconds...")
+			await send_to_discord(f"WebSocket error: {e}. Reconnecting in 5 seconds...")
+			await asyncio.sleep(5)
+		except Exception as e:
+			print(f"Unexpected error: {e}. Reconnecting in 10 seconds...")
+			await send_to_discord(f"Unexpected error: {e}. Reconnecting in 10 seconds...")
+			await send_to_discord(f"Unexpected error in twitch_chat_listener: {e}")
+			await asyncio.sleep(10)
 
 
 async def main():
-    retry_delay = 20
-    while True:
-        try:
-            print(f"Connecting to Twitch chat room {TARGET_ROOM_ID}")
-            print(f"Listening for messages from user {TARGET_USER_ID}")
-            await twitch_chat_listener()
-        except Exception as e:
-            error_message = f"An error occurred: {e}"
-            print(error_message)
-            await send_to_discord(error_message)
-            print(f"Retrying in {retry_delay} seconds...")
-            await asyncio.sleep(retry_delay)
+	retry_delay = 20
+	while True:
+		try:
+			print(f"Connecting to Twitch chat room {TARGET_ROOM_ID}")
+			print(f"Listening for messages from user {TARGET_USER_ID}")
+			await twitch_chat_listener()
+		except Exception as e:
+			error_message = f"An error occurred: {e}"
+			print(error_message)
+			await send_to_discord(error_message)
+			print(f"Retrying in {retry_delay} seconds...")
+			await asyncio.sleep(retry_delay)
 
 if __name__ == "__main__":
-    retry_delay = 20
-    while True:
-        try:
-            asyncio.run(main())
-        except Exception as e:
-            error_message = f"Critical error in main loop: {e}"
-            print(error_message)
-            try:
-                asyncio.run(send_to_discord(error_message))
-            except:
-                print("Failed to send error message to Discord")
-            print(f"Restarting the entire script in {retry_delay} seconds...")
-            time.sleep(retry_delay)
+	retry_delay = 20
+	while True:
+		try:
+			asyncio.run(main())
+		except Exception as e:
+			error_message = f"Critical error in main loop: {e}"
+			print(error_message)
+			try:
+				asyncio.run(send_to_discord(error_message))
+			except:
+				print("Failed to send error message to Discord")
+			print(f"Restarting the entire script in {retry_delay} seconds...")
+			time.sleep(retry_delay)

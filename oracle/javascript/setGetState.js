2cf7718c480be041266e019f90f04f81e748bd21
Guillaume Dumas <gdumas@z3r10p1.42lyon.fr>
2024-10-28 15:52:54 +0100

    fix: removed slice modif

--- oracle/javascript/setGetState.js
+++ oracle/javascript/setGetState.js
@@ -6,10 +6,8 @@ import { loadConfig } from './config.js';
 
 const config = loadConfig();
 
-function getInstructionIdentifier(instructionName) {
-	const hash = crypto.createHash('sha256').update(instructionName).digest(hex);
-	return hash.slice(0, 16);
-}
+const hash = crypto.createHash('sha256').update(instructionName).digest();
+	return hash.slice(0, 8);
 
 async function setGateState(open) {
 	const keypairFile = fs.readFileSync(config.oracle_wallet, 'utf-8');
